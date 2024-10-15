a43e616d199167b745c0c83f46f7b9a3850b2690
Vinit <vinits1014@gmail.com>
2024-10-15 09:22:33 +0530

    Updated code

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -6,7 +6,7 @@ const Button = () => {
     return (
         <div className="md:mt-6 mt-6">
             <div className="py-4 px-4 rounded-full border-2 bg-indigo-900/30 border-white shadow-white/50 shadow-lg relative w-max mt-5 md:mt-0">
-                <a href="https://drive.google.com/file/d/1U7x11fDLXnCbHFOe10M0BZZewRypjmYu/view?usp=drive_link" target="_blank" rel="noopener noreferrer">
+                <a href="https://drive.google.com/file/d/1TseeyO3gDalcumf7ioqbaW5XjuX8XCos/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                     <button
                         className="flex items-center"
                         onClick={() => {
